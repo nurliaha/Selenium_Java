245975ea58f348c181bff7d98bffa9520991c7b4
Nurliah Awaliah <nurliaha@gmail.com>
2020-06-26 23:55:33 +0700

    latihan dropdown

--- src/dropdownlat.java
+++ src/dropdownlat.java
@@ -1,23 +1,28 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class dropdownlat {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","C:\\\\SeleniumJava\\\\Chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\SeleniumJava\\Chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://spicejet.com");
-		driver.findElement(By.id("divpaxinfo")).click();
-			Thread.sleep(2000);
-		int i=1;
-		while(i<4) {
-			driver.findElement(By.id("ctl00_mainContent_ddl_Adult"));
-			i++;
-		}
-		driver.findElement(By.id("divpaxinfo")).click();
+		WebDriverWait myDynamicElement = new WebDriverWait(driver, 30);
+        myDynamicElement.until(ExpectedConditions.presenceOfElementLocated(By.id("ctl00_mainContent_ddl_originStation1_CTXT")));
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[@value='IXB']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@value='MAA']")).click();
+		Thread.sleep(2000);
+//		driver.findElement(By.id("ctl00_mainContent_ddl_destinationStation1_CTXT")).click();p
+	
 	}
 
 }
