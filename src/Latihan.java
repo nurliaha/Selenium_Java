8d1aacb913dbd7e7b5ba013a762299b6b941e860
Nurliah Awaliah <nurliaha@gmail.com>
2020-07-08 22:33:13 +0700

    Lat Assert Con

--- src/Latihan.java
+++ src/Latihan.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -8,7 +10,6 @@ import org.testng.annotations.Test;
 
 public class Latihan {
 //public static WebDriver driver;
-	@Test
 	public static void verifyHomepageTitle() throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://spicejet.com");
@@ -30,16 +31,67 @@ public class Latihan {
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//a[@value='IXB']")).click();
 		Thread.sleep(3000);
+//		id dynamic, so using index 
+//		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		
+//		alternatif id dynamic
+		
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+		Thread.sleep(2000);
+		
+//		Count the number of Checkboxes
+		
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		Thread.sleep(2000);
+		
+//		dropdown pessanger
+		
+		driver.findElement(By.xpath("//div[@id='ui-datepicker-div']")).click();
+//		driver.findElement(By.xpath("//a[@class='ui-state-default ui-state-active']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='divpaxinfo']")).click();
+		Thread.sleep(2000);
+		int i=1;
+		while(i<5) {
+			driver.findElement(By.xpath("//select[@id='ctl00_mainContent_ddl_Adult']")).sendKeys(Keys.DOWN);
+			i++;
+		}
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@id='divpaxinfo']")).getText(), "5 Adult");
+		System.out.println(driver.findElement(By.xpath("//div[@id='divpaxinfo']")).getText());
+//		Thread.sleep(3000);
+		
+		
+//		checkbox
+		
+//		WebElement chkbox = driver.findElement(By.xpath("//input[@id='ctl00_mainContent_chk_friendsandfamily']"));
+//		chkbox.click();
+//		System.out.println(chkbox.isSelected());
+//		Assert.assertFalse( driver.findElement(By.xpath("//input[@id='ctl00_mainContent_chk_SeniorCitizenDiscount']")).isSelected());
+//		Assert.assertFalse(false);
+//		WebElement chkbox2 = driver.findElement(By.xpath("//input[@id='ctl00_mainContent_chk_SeniorCitizenDiscount']"));
+//		chkbox2.click();
+//		System.out.println(chkbox2.isSelected());
+//		Assert.assertTrue(driver.findElement(By.xpath("//input[@id='ctl00_mainContent_chk_SeniorCitizenDiscount']")).isSelected());
+//		Assert.assertTrue(true);
+		
+		
+//		Thread.sleep(2000);
+//		after checkbox have other java script 
+
+//		driver.findElement(By.xpath("//div[@id='flightSearchContainer']")).click();
+		
+//		link
+		
+//		driver.findElement(By.partialLinkText("Special Assistance")).click();
+//		Thread.sleep(4000);
+//		driver.findElement(By.xpath("//a[@id='SpecialAssistanceWindow']")).click();
+//		Thread.sleep(2000);
+		driver.close();
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\SeleniumJava\\Chromedriver.exe");
 		verifyHomepageTitle();
-		
-		
-		
-		
 	}
-	
 }
